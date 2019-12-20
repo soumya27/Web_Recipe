e84a9a1db64bdccbd26d74dde8080a748cbf8694
soumya <soumya751@gmail.com>
2019-12-20 09:49:46 +0530

    Implementing the login page UI

--- final-project-team-node/recipe-app/src/app/login/login.component.ts
+++ final-project-team-node/recipe-app/src/app/login/login.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import {FormGroup, FormControl, Validators, FormGroupDirective, NgForm} from '@angular/forms';
+import {ErrorStateMatcher} from "@angular/material/core";
 
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,24 +16,26 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 })
 export class LoginComponent implements OnInit {
 
+  private imageUrl =
+    'url("https://cdn.pixabay.com/photo/2016/02/19/11/30/shrimp-1209744_960_720.jpg")';
   user = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl('')
+    email: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required])
   });
 
+  matcher = new MyErrorStateMatcher();
 
-  constructor(private fb: FormBuilder) {
+  updateWrapperBg(){
+    (<HTMLInputElement>document.getElementsByClassName("wrapper").item(0)).style.background = this.imageUrl ;
+    (<HTMLInputElement>document.getElementsByClassName("wrapper").item(0)).style.backgroundRepeat = 'no-repeat';
+    (<HTMLInputElement>document.getElementsByClassName("wrapper").item(0)).style.backgroundSize = 'cover';
   }
 
-  createForm() {
-    this.user = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
+  constructor() {
   }
-
 
   ngOnInit() {
+    this.updateWrapperBg();
   }
 
 
@@ -35,4 +45,5 @@ export class LoginComponent implements OnInit {
    */
   onUserLogin() {
   }
+
 }
